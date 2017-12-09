a82c4f5d3a2a29802eb91e077d9973f7243d8fcd
BerliozLeChat <neoflash93@gmail.com>
2017-12-09 15:18:18 +0100

    m2 configuration

---
+++ src/fr/univ/nantes/asa/m2/configuration/InterfaceConfiguration.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package fr.univ.nantes.asa.m2.configuration;
+
+import java.util.Collection;
+
+/**
+ * @author francois
+ *
+ */
+public abstract class InterfaceConfiguration {
+
+	Collection<PortConfigurationRequis> portRequis;
+	Collection<PortConfigurationFournis> portFournis;
+}
