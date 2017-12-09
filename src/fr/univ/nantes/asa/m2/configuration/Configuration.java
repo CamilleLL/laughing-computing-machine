d6f08e660cd40273b52a837e507e793a747fabbc
BerliozLeChat <neoflash93@gmail.com>
2017-12-09 15:18:03 +0100

    m2 configuration

--- src/fr/univ/nantes/asa/m2/configuration/Configuration.java
+++ src/fr/univ/nantes/asa/m2/configuration/Configuration.java
@@ -3,12 +3,21 @@
  */
 package fr.univ.nantes.asa.m2.configuration;
 
+import java.util.Collection;
+
 import fr.univ.nantes.asa.m2.IArchitecturalElement;
+import fr.univ.nantes.asa.m2.composant.Composant;
+import fr.univ.nantes.asa.m2.connecteur.Connecteur;
 
 /**
  * @author francois
  *
  */
 public abstract class Configuration implements IArchitecturalElement{
-
+	
+	Collection<Composant> composants;
+	Collection<Connecteur> connecteurs;
+	Collection<Attachement> attachements;
+	Collection<Binding> bindings;
+	Collection<InterfaceConfiguration> interfaces;
 }
