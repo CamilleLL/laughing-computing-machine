cfa69a5951594e86f975a83d4e8625f95792c42c
BerliozLeChat <neoflash93@gmail.com>
2017-12-10 14:10:02 +0100

    composants et interfaces

--- src/fr/univ/nantes/asa/m1/serveurdetail/composant/Database.java
+++ src/fr/univ/nantes/asa/m1/serveurdetail/composant/Database.java
@@ -3,12 +3,28 @@
  */
 package fr.univ.nantes.asa.m1.serveurdetail.composant;
 
+import java.util.ArrayList;
+
 import fr.univ.nantes.asa.m2.composant.Composant;
+import fr.univ.nantes.asa.m2.composant.InterfaceComposant;
+import fr.univ.nantes.asa.m2.composant.PortComposantFournis;
+import fr.univ.nantes.asa.m2.composant.PortComposantRequis;
 
 /**
  * @author francois
  *
  */
 public class Database extends Composant {
+	public Database() {
+		this.interfaces = new ArrayList<InterfaceComposant>();
+		this.interfaces.add(new IDatabase(this,new PortComposantRequis(), new PortComposantFournis()));
+	}
+	
+	public PortComposantRequis getPortRequis() {
+		return this.interfaces.get(0).portRequis;
+	}
 
+	public PortComposantFournis getPortFournis() {
+		return this.interfaces.get(0).portFournis;
+	}
 }
---
+++ src/fr/univ/nantes/asa/m1/serveurdetail/composant/ISecurityManager.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package fr.univ.nantes.asa.m1.serveurdetail.composant;
+
+import fr.univ.nantes.asa.m2.composant.Composant;
+import fr.univ.nantes.asa.m2.composant.InterfaceComposant;
+import fr.univ.nantes.asa.m2.composant.PortComposantFournis;
+import fr.univ.nantes.asa.m2.composant.PortComposantRequis;
+
+/**
+ * @author francois
+ *
+ */
+public class ISecurityManager extends InterfaceComposant {
+
+	public ISecurityManager(Composant c, PortComposantRequis p1, PortComposantFournis p2) {
+		super(c, p1, p2);
+	}
+
+}
